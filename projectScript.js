0cf94fac1e88a0604a66dbc1ea3787067c9efafd
deablosf <cars9156@gmail.com>
2022-01-12 20:41:28 -0500

    place holder js stuff

--- projectScript.js
+++ projectScript.js
@@ -1,9 +1,9 @@
 const eximList = ["Amalgom", "Cerbo", "Kin", "Proteans", "Qaurk"]
 
 const eximDetails = {
-    Amalgom: "",
-    Cerbo: "",
-    Kin: "",
-    Protean: "",
-    Quark: ""
+    Amalgom: "They got all the flavors but a stranger!",
+    Cerbo: "They brains scream at you, till you cry!",
+    Kin: "They heavy, you heavy and we all fall up!",
+    Protean: "That's no bat that's Jamal with a RPG!",
+    Quark: "AVATAR Mutha FUGDER!"
 }
