99f64ca945476b613855fd00969db331dac92998
deablosf <cars9156@gmail.com>
2022-01-14 01:00:36 -0500

    Map in place buttons need to be moved into place

--- projectScript.js
+++ projectScript.js
@@ -8,12 +8,14 @@ const eximDetails = {
     Quark: "AVATAR Mutha FUGDER!"
 }
 
+const boroughList = ["Bronx", "Brooklyn", "Manhattan", "Queens", "Staten"]
+
 const boroughDetails = {
-    Bronx: "",
-    Brooklyn: "",
-    Manhattan: "",
-    Queens: "",
-    Staten: ""
+    Bronx: "It's burning Baby!",
+    Brooklyn: "It's Hipsters Baby!",
+    Manhattan: "It's Tourists Baby!",
+    Queens: "IT smells like something's cooking Baby",
+    Staten: "No ..."
 }
 
 const eximDetailsHtml = document.getElementById('eximDetails');
@@ -25,17 +27,24 @@ const eximDetschanger = (z) => {
     eximDetailsHtml.innerText = eximDetails[z]
  };
 
- const boroughDetsHtml = (z) => {
+const boroughDetsHtml = (z) => {
     boroughDetailsHtml.innerText = boroughDetails[z]
- };
+};
 
- let Classification = () => {
+let classification = () => {
     eximClassHtml.innerHTML = eximList.map(classi => 
         '<button class="eximbuttons" onclick="eximDetschanger(' + `'${classi}'` + ')">' + classi.replace(/_/g, ' ') + '</button>'
         ).join('');
         console.log("I'm on")
- }
+};
 
+// let boroughDescrip = () => {
+//     theCityImHtml.innerHTML = boroughList.map(boro => 
+//         '<button class="borougButtons" onclick="boroughDetsHtml(' + `'${boro}'` + ')">' + boro.replace(/_/g, ' ') + '</button>'
+//         ).join('');
+//         console.log("I'm on")
+// }
  
 
- Classification()
+ classification()
+ // boroughDescrip()
